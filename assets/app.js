744bd1a2d235ffdc1e54c8a8fc08fdc62fa6ed73
Jesús García Crespo <jesus@sevein.com>
2015-03-21 11:16:53 -0700

    frontend: basic list

--- assets/app.js
+++ assets/app.js
@@ -2,16 +2,29 @@
 
 var app = angular.module('guggmeta', []);
 
-app.controller('MainCtrl', function MainCtrl($scope, $timeout) {
+app.controller('MainCtrl', function MainCtrl($scope, SubmissionService) {
 
-  $scope.time = 'time...'
-  var interval = 100;
+  $scope.submissions = [];
 
-  var tick = function () {
-    $scope.time = Date.now();
-    $timeout(tick, interval);
-  }
+  var load = function () {
+    SubmissionService.getAll().then(function (response) {
+      $scope.submissions = response.data.hits;
+      console.log(response.data);
+    });
+  };
 
-  $timeout(tick, interval);
+  load();
+
+});
+
+app.service('SubmissionService', function SubmissionService($http) {
+
+  this.getOne = function (id) {
+    return $http.get('/api/submission/' + id + '/');
+  };
+
+  this.getAll = function () {
+    return $http.get('/api/submissions/');
+  };
 
 });
