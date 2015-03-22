64efc529f8170c9c696d0a227bc9f731272d20b2
Jesús García Crespo <jesus@sevein.com>
2015-03-22 00:50:26 -0700

    frontend: basic functionality, routing

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,42 @@
 'use strict';
 
-var app = angular.module('guggmeta', []);
+var app = angular.module('guggmeta', ['ngMaterial', 'ngRoute', 'ngAria', 'ngAnimate']);
 
-app.controller('MainCtrl', function MainCtrl($scope, SubmissionService) {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/submissions', {
+      templateUrl: '/assets/partials/submission-list.tmpl.html',
+      controller: 'SubmissionListCtrl'
+    })
+    .when('/submissions/:id', {
+      templateUrl: '/assets/partials/submission-detail.tmpl.html',
+      controller: 'SubmissionDetailCtrl'
+    })
+    .otherwise('/submissions');
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
 
-  $scope.submissions = [];
+app.controller('SubmissionListCtrl', ['$scope', 'SubmissionService', function ($scope, SubmissionService) {
+  SubmissionService.getAll().then(function (response) {
+    $scope.submissions = response.data.results;
+  });
+}]);
 
-  var load = function () {
-    SubmissionService.getAll().then(function (response) {
-      $scope.submissions = response.data.hits;
-      console.log(response.data);
-    });
-  };
+app.controller('SubmissionDetailCtrl', ['$scope', '$routeParams', 'SubmissionService', function ($scope, $routeParams, SubmissionService) {
+  $scope.id = $routeParams.id;
+  SubmissionService.getOne($routeParams.id).then(function (response) {
+    $scope.submission = response.data;
+  });
+}]);
 
-  load();
-
-});
-
-app.service('SubmissionService', function SubmissionService($http) {
-
+app.service('SubmissionService', ['$http', function ($http) {
   this.getOne = function (id) {
-    return $http.get('/api/submission/' + id + '/');
+    return $http.get('/api/submissions/' + id + '/');
   };
-
   this.getAll = function () {
     return $http.get('/api/submissions/');
   };
-
-});
+}]);
